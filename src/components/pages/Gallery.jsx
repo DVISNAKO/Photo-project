6dcfedb89dfa5e5fbde91882b90c59ea4e62cc2b
Dima <dmitrijs.visnakovs@inbox.lv>
2024-03-21 16:34:39 +0200

    galery code refactoring

--- src/components/pages/Gallery.jsx
+++ src/components/pages/Gallery.jsx
@@ -1,66 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { ROUTES } from "../../utils/routes";
-import IMG1 from "./images/sieviesu/s1.jpeg";
-import IMG2 from "./images/dzivnieki/d6.jpeg";
-import IMG3 from "./images/viriesi/m5.jpeg";
-import IMG4 from "./images/pasakumi/p1.jpeg";
+import { galeryData } from "../../utils/constants";
 
 const Gallery = () => {
   return (
     <div className="flex justify-center items-center h-full my-10 flex-wrap gap-5 mx-10 border-black">
-      <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
-        <Link to={ROUTES.SIEVEŠU}>
-          <img
-            className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px]"
-            src={IMG1}
-          />
-          <h2 className="flex justify-center mt-5 ">SIEVIEŠU FOTO</h2>
-        </Link>
-      </div>
-      <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
-        <Link to={ROUTES.DZIVNIEKI}>
-          <img
-            className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px]"
-            src={IMG2}
-          />
-          <h2 className="flex justify-center mt-5 ">DZĪVNIEKU FOTO</h2>
-        </Link>
-      </div>
-      <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
-        <Link to={ROUTES.VIRIESI}>
-          <img
-            className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px]"
-            src={IMG3}
-          />
-          <h2 className="flex justify-center mt-5 ">VĪRIEŠU FOTO</h2>
-        </Link>
-      </div>
-      <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
-        <Link to={ROUTES.PASAKUMI}>
-          <img
-            className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px] "
-            src={IMG4}
-          />
-          <h2 className="flex justify-center mt-5 ">PASĀKUMU FOTO</h2>
-        </Link>
-      </div>
-      <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
-        <img
-          className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px]"
-          src="https://images.unsplash.com/photo-1621012430307-b4774b78d3cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        />
-        <h2 className="flex justify-center mt-5 ">VIDEO PORT</h2>
-      </div>
-      <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
-        <Link to={ROUTES.GRUPAS}>
-          <img
-            className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px]"
-            src="https://images.unsplash.com/photo-1621012430307-b4774b78d3cb?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-          />
-          <h2 className="flex justify-center mt-5 ">GRUPAS FOTO</h2>
-        </Link>
-      </div>
+      {galeryData.map((item) => (
+        <div className="cursor-pointer hover:translate-y-2 hover:text-orange-400 basis-1/4">
+          <Link to={item.route}>
+            <img
+              className="flex justify-center items-center object-cover max-h-[600px] max-w-[500px]"
+              src={item.img}
+            />
+            <h2 className="flex justify-center mt-5 ">{item.title}</h2>
+          </Link>
+        </div>
+      ))}
     </div>
   );
 };
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,10 @@
-// объект для пополнения страниц Pice, подолнять предложения на страницу
+import IMG1 from "../components/pages/images/sieviesu/s1.jpeg";
+import IMG2 from "../components/pages/images/dzivnieki/d6.jpeg";
+import IMG3 from "../components/pages/images/viriesi/m5.jpeg";
+import IMG4 from "../components/pages/images/pasakumi/p1.jpeg";
+import { ROUTES } from "../utils/routes";
 
+// объект для пополнения страниц Pice, подолнять предложения на страницу
 export const packageData = [
     {
       title: "Package 1",
@@ -21,10 +26,45 @@ export const packageData = [
     },
   ];
 
+  //название кнопок меню
   export const menu = [
     { title: "Gallery", id: 1 },
     { title: "About", id: 2 },
     { title: "Price", id: 3 },
     { title: "Contacts", id: 4 },
     { title: "Partners", id: 5 },
+  ];
+
+  //вывод на страницу галереи
+  export const galeryData = [
+    {
+      title: "SIEVIEŠU FOTO",
+      img: IMG1,
+      route: ROUTES.SIEVEŠU,
+    },
+    {
+      title: "DZĪVNIEKU FOTO",
+      img: IMG2,
+      route: ROUTES.DZIVNIEKI,
+    },
+    {
+      title: "VĪRIEŠU FOTO",
+      img: IMG3,
+      route: ROUTES.VIRIESI,
+    },
+    {
+      title: "PASĀKUMU FOTO",
+      img: IMG4,
+      route: ROUTES.PASAKUMI,
+    },
+    {
+      title: "VIDEO PORT",
+      img: IMG4,
+      route: ROUTES.PASAKUMI,
+    },
+    {
+      title: "GRUPAS FOTO",
+      img: IMG4,
+      route: ROUTES.GRUPAS,
+    },
   ];
