2f1d28db1fda43410077c6be3991f09e288720db
Dima <dmitrijs.visnakovs@inbox.lv>
2024-04-10 16:36:29 +0300

    price

--- src/components/SinglePage/SinglePrice.jsx
+++ src/components/SinglePage/SinglePrice.jsx
@@ -1,17 +1,30 @@
 import React, { useContext } from "react";
 import { LanguageState } from "../../utils/context";
-import './Button.css';
+import "./Button.css";
 
 const singlePrice = ({ packageData }) => {
-  const { title, idea, time, curruncy, gift, price, img1, img2, titleRus, ideaRus } = packageData;
+  const {
+    title,
+    idea,
+    time,
+    curruncy,
+    gift,
+    price,
+    img1,
+    img2,
+    titleRus,
+    ideaRus,
+    bonuss,
+    bonussRus,
+    curruncyRus,
+    priceRU,
+  } = packageData;
   const [language, setLanguage] = useContext(LanguageState);
 
   return (
     <>
       <div className="flex justify-center items-center mt-5 flex-col mx-5">
-        <h2 className="mb-5 font-bold">
-          {language ? title : titleRus}
-         </h2>
+        <h2 className="mb-5 font-bold">{language ? title : titleRus}</h2>
         <div className="flex justify-center flex-row gap-5 mx-5">
           <img
             className="w-1/2 max-h-[400px] max-w-[300px] object-cover hover:translate-y-2"
@@ -27,27 +40,32 @@ const singlePrice = ({ packageData }) => {
         {language ? (
           <>
             <h2 className="mb-3 font-bold">{title}</h2>
-            <li>{idea}</li>
-            <li>{time}</li>
-            <li>{curruncy}</li>
-            <li>{price}</li>
+            {idea && <li>{idea}</li>}
+            {time && <li>{time}</li>}
+            {curruncy && <li>{curruncy}</li>}
+            {price && <li>{price}</li>}  
+            {bonuss && <li>{bonuss}</li>}
           </>
         ) : (
           <>
             <h2 className="mb-3 font-bold">{titleRus}</h2>
-            <li>{ideaRus}</li>
-            <li>{time}</li>
-            <li>{curruncy}</li>
-            <li>{price}</li>
+            {ideaRus && <li>{ideaRus}</li>}
+            {time && <li>{time}</li>}
+            {curruncyRus && <li>{curruncyRus}</li>}
+            {price && <li>{price}</li>}
+            {bonussRus && <li>{bonussRus}</li>}
           </>
         )}
       </div>
-      <div className="flex justify-center text-center mt-5">  <h2 className="BtnAnimation w-[200px] m-2 border-2 p-3 rounded-xl hover:bg-slate-400 bg-rose-300 ">
+      <div className="flex justify-center text-center mt-5">
+        {" "}
+        <h2 className="BtnAnimation w-[200px] m-2 border-2 p-3 rounded-xl hover:bg-slate-400 bg-rose-300 ">
           <a target="_blank" href="https://t.me/lainevolcite">
-          {language ? <>Saziņai telegramā</> : <>Написать в телеграм</> }
+            {language ? <>Saziņai telegramā</> : <>Написать в телеграм</>}
           </a>
-        </h2></div>
-    
+        </h2>
+      </div>
+
       <hr className="my-4 h-0,5 bg-slate-200" />
     </>
   );
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,88 +5,130 @@ import IMG4 from "../components/pages/images/Gallery/ga4.jpeg";
 import IMG5 from "../components/pages/images/Gallery/ga5.jpeg";
 import IMG6 from "../components/pages/images/Gallery/ga6.jpeg";
 import { ROUTES } from "../utils/routes";
-import PRICE1 from '../components/pages/images/Price/pa1.jpeg'
-import PRICE2 from '../components/pages/images/Price/pa2.jpeg'
+import PRICE1_1 from "../components/pages/images/Price/pa1.jpeg";
+import PRICE1_2 from "../components/pages/images/Price/pa2.jpeg";
+import PRICE2_1 from "../components/pages/images/pasakumi/p1.jpeg";
+import PRICE2_2 from "../components/pages/images/pasakumi/p2.jpeg";
+import PRICE3_1 from "../components/pages/images/love/lo1.jpeg";
+import PRICE3_2 from "../components/pages/images/love/lo2.jpeg";
+import PRICE4_1 from "../components/pages/images/dzivnieki/dz9.jpeg";
+import PRICE4_2 from "../components/pages/images/dzivnieki/d6.jpeg";
+import PRICE5_1 from '../components/pages/images/grupas/GIM1.jpeg'
+import PRICE5_2 from '../components/pages/images/grupas/GIM2.jpeg'
 
 // объект для пополнения страниц Pice, подолнять предложения на страницу
 export const packageData = [
-    {
-      title: "Individuālā fotosesija",
-      titleRus: 'Индивидуальная фотосессия',
-      ideaRus: 'идея',
-      idea: "ideja",
-      time: "~2h",
-      curruncy: "15 fotogrāfijas",
-      price: "50€",
-      img1: PRICE1,
-      img2: PRICE2,
-    },
-    {
-        title: "Individuālā fotosesija",
-        titleRus: 'Индивидуальная фотосессия',
-        ideaRus: 'идея',
-        idea: "ideja",
-        time: "~2h",
-        curruncy: "15 fotogrāfijas",
-        price: "60€",
-        img1: PRICE1,
-        img2: PRICE2,
-    },
-  ];
+  {
+    title: "Individuālā fotosesija",
+    titleRus: "Индивидуальная фотосессия",
+    ideaRus: "идея",
+    idea: "ideja",
+    curruncy: "20 fotogrāfijas",
+    curruncyRus: "20 фотографий",
+    price: "60€",
+    bonuss: "+video portrets",
+    bonussRus: '+ видео портрет',
+    img1: PRICE1_1,
+    img2: PRICE1_2,
+  },
+  {
+    title: "Love story",
+    titleRus: "Love story",
+    ideaRus: "идея",
+    idea: "ideja",
+    curruncy: "20 fotogrāfijas",
+    curruncyRus: "20 фотографий",
+    price: "70€",
+    bonuss: "+video",
+    bonussRus: '+ видео',
+    img1: PRICE3_1,
+    img2: PRICE3_2,
+  },
+  {
+    title: "Pasākumu foto",
+    titleRus: "Фотосьемка мероприятия",
+    time: "~2h - 30€",
+    price: "visa diena/весь день 150€",
+    img1: PRICE2_1,
+    img2: PRICE2_2,
+  },
+  {
+    title: "Dzīvnieku foto",
+    titleRus: "Фотосьемка животных",
+    curruncy: "30 fotogrāfijas",
+    curruncyRus: "30 фотографий",
+    price: "60€",
+    bonuss: "+ fotogrāfijas ar saimnieku",
+    bonussRus: '+ фото с хозяином',
+    img1: PRICE4_1,
+    img2: PRICE4_2,
+  },
+  {
+    title: "Ğimenes fotosesija",
+    titleRus: "Семейная фотосессия",
+    ideaRus: "идея",
+    idea: "ideja",
+    curruncy: "20-30 fotogrāfijas",
+    curruncyRus: "20-30 фотографий",
+    price: "80€",
+    img1: PRICE5_1,
+    img2: PRICE5_2,
+  },
+];
 
-  //название кнопок меню
-  export const menu = [
-    { title: "GALERIJA", id: 1 },
-    { title: "PAR_MANI", id: 2 },
-    { title: "CENAS", id: 3 },
-    { title: "KONTAKTI", id: 4 },
-    { title: "SASNIEGUMI", id: 5 },
-  ];
+//название кнопок меню
+export const menu = [
+  { title: "GALERIJA", id: 1 },
+  { title: "PAR_MANI", id: 2 },
+  { title: "CENAS", id: 3 },
+  { title: "KONTAKTI", id: 4 },
+  { title: "SASNIEGUMI", id: 5 },
+];
 
-  export const menuRUS = [
-    { title: "ГАЛЕРЕЯ", id: 1 },
-    { title: "ОБО МНЕ", id: 2 },
-    { title: "ЦЕНЫ", id: 3 },
-    { title: "КОНТАКТЫ", id: 4 },
-    { title: "ДОСТИЖЕНИЯ", id: 5 },
-  ]
+export const menuRUS = [
+  { title: "ГАЛЕРЕЯ", id: 1 },
+  { title: "ОБО МНЕ", id: 2 },
+  { title: "ЦЕНЫ", id: 3 },
+  { title: "КОНТАКТЫ", id: 4 },
+  { title: "ДОСТИЖЕНИЯ", id: 5 },
+];
 
-  //вывод на страницу галереи
-  export const galeryData = [
-    {
-      title: "SIEVIEŠU FOTO",
-      titleRus: "ЖЕНСКИЕ ФОТО",
-      img: IMG1,
-      route: ROUTES.SIEVIEŠU,
-    },
-    {
-      title: "DZĪVNIEKU FOTO",
-      titleRus: "ФОТО ЖИВОТНЫХ",
-      img: IMG2,
-      route: ROUTES.DZIVNIEKI,
-    },
-    {
-      title: "VĪRIEŠU FOTO",
-      titleRus: "МУЖСКИЕ ФОТО",
-      img: IMG3,
-      route: ROUTES.VIRIESI,
-    },
-    {
-      title: "PASĀKUMU FOTO",
-      titleRus: "ФОТО МЕРОПРИЯТИЙ",
-      img: IMG4,
-      route: ROUTES.PASAKUMI,
-    },
-    {
-      title: "LOVE STORY",
-      titleRus: "ФОТО ИСТОРИЙ",
-      img: IMG5,
-      route: ROUTES.LOVE,
-    },
-    {
-      title: "ĢIMENES FOTOSESIJA",
-      titleRus: "СЕМЕЙНЫЕ ФОТО",
-      img: IMG6,
-      route: ROUTES.GRUPAS,
-    },
-  ];
+//вывод на страницу галереи
+export const galeryData = [
+  {
+    title: "SIEVIEŠU FOTO",
+    titleRus: "ЖЕНСКИЕ ФОТО",
+    img: IMG1,
+    route: ROUTES.SIEVIEŠU,
+  },
+  {
+    title: "DZĪVNIEKU FOTO",
+    titleRus: "ФОТО ЖИВОТНЫХ",
+    img: IMG2,
+    route: ROUTES.DZIVNIEKI,
+  },
+  {
+    title: "VĪRIEŠU FOTO",
+    titleRus: "МУЖСКИЕ ФОТО",
+    img: IMG3,
+    route: ROUTES.VIRIESI,
+  },
+  {
+    title: "PASĀKUMU FOTO",
+    titleRus: "ФОТО МЕРОПРИЯТИЙ",
+    img: IMG4,
+    route: ROUTES.PASAKUMI,
+  },
+  {
+    title: "LOVE STORY",
+    titleRus: "LOVE STORY",
+    img: IMG5,
+    route: ROUTES.LOVE,
+  },
+  {
+    title: "ĢIMENES FOTOSESIJA",
+    titleRus: "СЕМЕЙНЫЕ ФОТО",
+    img: IMG6,
+    route: ROUTES.GRUPAS,
+  },
+];
